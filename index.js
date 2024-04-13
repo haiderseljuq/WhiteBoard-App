d3d61cd9212195f0d5b4b881537a06da5ff78f76
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-13 17:14:54 -0400

    chore: Uploading initial outline

---
+++ index.js
@@ -0,0 +1,11 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data); // receive a hint 
+  });
+
+  ws.send('something'); // Send picture to openAI
+});
